e4f09150d36c69c26a35740670e6d3e25fe0dfc1
Imara Dharmakirthi <149819817+Ace1032@users.noreply.github.com>
2024-04-25 02:58:46 -0700

    Update Quack_data_structure.java

--- Quack_data_structure.java
+++ Quack_data_structure.java
@@ -1,3 +1,24 @@
+/*
+ * This problem was asked by Google.
+
+A quack is a data structure combining properties of both stacks and queues. 
+It can be viewed as a list of elements written left to right such that three operations are possible:
+
+push(x): add a new item x to the left end of the list
+pop(): remove and return the item on the left end of the list
+pull(): remove the item on the right end of the list.
+
+Implement a quack using three stacks and O(1) additional memory, so that the amortized 
+time for any push, pop, or pull operation is O(1).
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+
 package solutions;
 import java.util.ArrayList;
 import java.io.DataInputStream;
@@ -98,11 +119,9 @@ class Quack{
 	}
 	
 	public void pull() {
-		//System.out.println(last.x);
 		Node temp= first;
 		while(temp != last) {
 			temp=temp.next;
-			//System.out.print(temp.x+ " ");
 			if(temp.next== last) {
 				last= temp;
 				temp.next=null;
@@ -119,7 +138,5 @@ class Quack{
 		System.out.println();
 	}
 	
-	
-	
 		
-	}
+}
